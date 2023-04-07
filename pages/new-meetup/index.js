6e1c8938fd7900232a4ac178e445a121d1fb1bdd
hadarean <Ioan-Dumitru.Hadarean@arrk-engineering.com>
2023-04-07 14:08:12 +0300

    Added demo project for next-js

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,36 @@
+//our-domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
+
+function NewMeetupPage() {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name="description" content="Browse a huge list of meetups!"></meta>
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
